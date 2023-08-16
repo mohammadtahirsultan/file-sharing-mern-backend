5a6a7bd48050fafe43f505bd610e0b58166dca70
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-08-15 21:17:04 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './database/db.js';
+import router from './routes/file.js';
+import upload from './multer/multer.js';
+import { uploadFile } from './controller/file.js';
+import cors from 'cors'
+const app = express()
+
+dotenv.config({ path: "./config.env" })
+const port = 4000;
+
+connectDB()
+
+
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
+
+app.use(router)
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+
+app.listen(port, (req, res) => {
+    console.log("App is Running");
+})
